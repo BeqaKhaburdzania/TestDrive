644a795542767cd576120526193b660d281078e0
bkhab12@freeuni.edu.ge <bkhab12@freeuni.edu.ge>
2018-05-29 20:28:24 +0400

    axla?

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import {create as createApp} from './app/server';
-import {getBalance, getBalanceFull} from './app/fetcher';
+import {getBalance} from './app/fetcher';
 
 const port = process.env.PORT || 3000;
 const mainNet = 'MainNet';
 
-const app = createApp({net: mainNet, request: getBalanceFull});
+const app = createApp({net: mainNet, request: getBalance});
 
 app.listen(port, () => console.log("Server is listening on port 3000"));
